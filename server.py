7d408dd7ac4f2fefb4238e594998549dfbf2c7c7
nindyasaridu <nindyasari13@mhs.if.its.ac.id>
2016-03-10 11:32:48 +0000

    edit server rmi

--- server.py
+++ server.py
@@ -7,22 +7,6 @@ class MyService(rpyc.Service):
 	global logevent
 	def exposed_line_counter(self, fileobj) :
 		for line in fileobj:	
-			#print line
-			#hasil = line.split()
-			#hasil = hasil[3]+" "+hasil[4]		
-		#with open(fileobj,'r') as inifile :
-			#baca = str( inifile.read()).split('\n')
-			#panjang = len(baca)-1
-			#panjang = len(line)			
-			#print panjang
-			
-			#for i in range(panjang) :
-			#	split1 = baca[i].split()[3:5]
-			#	event = " ".join(split1)
-			#	if event in logevent :
-			#		logevent[event] +=1
-			#	else :
-			#		logevent[event] = 1
 			split1 = line.split()
 			event = split1[3]+" "+split1[4]
 			if event not in logevent:
